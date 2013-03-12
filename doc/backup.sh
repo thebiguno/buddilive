fc3705d69ef71e90569c2211c149a7c0456322aa
Wyatt <wyatt@digitalcave.ca>
2013-03-11 21:08:39 -0600

    Added test files to help debug account balance addition problems

--- doc/backup.sh
+++ doc/backup.sh
@@ -1,4 +1,4 @@
 RESTORE=doc/restore.sh
 echo "echo '" > $RESTORE
-curl -u "foo@example.com:password" "http://localhost:8080/data/backup" | jsonlint >> $RESTORE
-echo "' | curl -d @- -u 'foo@example.com:password' http://localhost:8080/data/restore | jsonlint; echo ''" >> $RESTORE
+curl -u "wyatt.olson@gmail.com:password" "http://localhost:8080/data/backup" | jsonlint >> $RESTORE
+echo "' | curl -d @- -u 'wyatt.olson@gmail.com:password' http://localhost:8080/data/restore | jsonlint; echo ''" >> $RESTORE
--- doc/restore.sh
+++ doc/restore.sh
@@ -3,173 +3,264 @@ echo '
     "accounts": [
         {
             "accountType": "Cash", 
-            "name": "Cash in Wallet", 
-            "startBalance": "120", 
-            "startDate": "1900-01-01", 
+            "name": "My Wallet", 
+            "startBalance": "120.00", 
+            "startDate": "2013-03-11", 
             "type": "D", 
-            "uuid": "3a26c3ea-f101-4f35-86a7-7b3ba438a79e"
+            "uuid": "AccountImpl-13d5c65dd62-224-3b69"
         }, 
         {
             "accountType": "Chequing", 
-            "name": "RBC Chequing", 
-            "startBalance": "1404", 
-            "startDate": "1900-01-01", 
+            "name": "TD Chequing", 
+            "startBalance": "3012.42", 
+            "startDate": "2013-03-03", 
             "type": "D", 
-            "uuid": "c29d0f85-da86-4bde-b656-3ba6350f499f"
+            "uuid": "AccountImpl-13d5c695c01-7de8-1251"
         }, 
         {
             "accountType": "Credit Card", 
-            "name": "Visa Gold", 
-            "startBalance": "532", 
-            "startDate": "1900-01-01", 
+            "name": "Visa", 
+            "startBalance": "0.00", 
+            "startDate": "2013-03-11", 
             "type": "C", 
-            "uuid": "56a6405f-7f45-4505-a2f5-22d396478e61"
+            "uuid": "AccountImpl-13d5c698d77-d029-f117"
         }
     ], 
     "categories": [
         {
-            "categories": [
-                {
-                    "name": "Bonus", 
-                    "parent": "c3231a22-cab8-4e90-955a-219500320900", 
-                    "periodType": "MONTH", 
-                    "startDate": "1900-01-01T00:00:00", 
-                    "type": "I", 
-                    "uuid": "6e4b612b-272f-42f0-a280-1701946ebf1e"
-                }
-            ], 
-            "name": "Salary", 
+            "name": "Bonus", 
             "periodType": "MONTH", 
-            "startDate": "1900-01-01T00:00:00", 
             "type": "I", 
-            "uuid": "c3231a22-cab8-4e90-955a-219500320900"
+            "uuid": "BudgetCategoryImpl-13d5c659e0b-b66a-45dd"
         }, 
         {
-            "categories": [
-                {
-                    "name": "Fuel", 
-                    "parent": "138b95ff-4425-4eaa-9997-627c4f230a6a", 
-                    "periodType": "MONTH", 
-                    "startDate": "1900-01-01T00:00:00", 
-                    "type": "E", 
-                    "uuid": "d52f3bce-72bc-42fb-a80a-4f9aaef1e95f"
-                }, 
-                {
-                    "name": "Insurance", 
-                    "parent": "138b95ff-4425-4eaa-9997-627c4f230a6a", 
-                    "periodType": "MONTH", 
-                    "startDate": "1900-01-01T00:00:00", 
-                    "type": "E", 
-                    "uuid": "e06624e0-6221-4574-9579-7f57466416ed"
-                }
-            ], 
+            "name": "Investment Income", 
+            "periodType": "MONTH", 
+            "type": "I", 
+            "uuid": "BudgetCategoryImpl-13d5c659e0b-94ce-1108"
+        }, 
+        {
+            "name": "Salary", 
+            "periodType": "MONTH", 
+            "type": "I", 
+            "uuid": "BudgetCategoryImpl-13d5c659e0b-952e-b78b"
+        }, 
+        {
             "name": "Auto", 
             "periodType": "MONTH", 
-            "startDate": "1900-01-01T00:00:00", 
             "type": "E", 
-            "uuid": "138b95ff-4425-4eaa-9997-627c4f230a6a"
+            "uuid": "BudgetCategoryImpl-13d5c659e0a-481f-b7c9"
+        }, 
+        {
+            "name": "Entertainment", 
+            "periodType": "MONTH", 
+            "type": "E", 
+            "uuid": "BudgetCategoryImpl-13d5c659e0a-dd7a-1fc3"
         }, 
         {
             "name": "Groceries", 
             "periodType": "MONTH", 
-            "startDate": "1900-01-01T00:00:00", 
             "type": "E", 
-            "uuid": "4378588d-e8ef-443b-88f2-81fc9277ac46"
+            "uuid": "BudgetCategoryImpl-13d5c659e0a-8e20-e5c"
+        }, 
+        {
+            "name": "Household", 
+            "periodType": "MONTH", 
+            "type": "E", 
+            "uuid": "BudgetCategoryImpl-13d5c659e0a-cb-662c"
+        }, 
+        {
+            "name": "Investment Expenses", 
+            "periodType": "MONTH", 
+            "type": "E", 
+            "uuid": "BudgetCategoryImpl-13d5c659e0a-60e1-be7f"
+        }, 
+        {
+            "name": "Misc. Expenses", 
+            "periodType": "MONTH", 
+            "type": "E", 
+            "uuid": "BudgetCategoryImpl-13d5c659e0a-d2a0-9e81"
+        }, 
+        {
+            "name": "Utilities", 
+            "periodType": "MONTH", 
+            "type": "E", 
+            "uuid": "BudgetCategoryImpl-13d5c659e0a-bf50-50d8"
         }
     ], 
     "entries": [
         {
-            "amount": "500.00", 
-            "category": "4378588d-e8ef-443b-88f2-81fc9277ac46", 
+            "amount": "100.00", 
+            "category": "BudgetCategoryImpl-13d5c659e0b-b66a-45dd", 
             "date": "2013-03-01"
         }, 
         {
-            "amount": "100.00", 
-            "category": "d52f3bce-72bc-42fb-a80a-4f9aaef1e95f", 
-            "date": "2013-03-01"
+            "amount": "500.00", 
+            "category": "BudgetCategoryImpl-13d5c659e0b-b66a-45dd", 
+            "date": "2013-04-01"
         }, 
         {
             "amount": "200.00", 
-            "category": "e06624e0-6221-4574-9579-7f57466416ed", 
-            "date": "2013-03-01"
-        }, 
-        {
-            "amount": "100.00", 
-            "category": "6e4b612b-272f-42f0-a280-1701946ebf1e", 
-            "date": "2013-03-01"
+            "category": "BudgetCategoryImpl-13d5c659e0b-94ce-1108", 
+            "date": "2013-04-01"
         }, 
         {
             "amount": "1000.00", 
-            "category": "c3231a22-cab8-4e90-955a-219500320900", 
+            "category": "BudgetCategoryImpl-13d5c659e0b-952e-b78b", 
+            "date": "2013-03-01"
+        }, 
+        {
+            "amount": "800.00", 
+            "category": "BudgetCategoryImpl-13d5c659e0b-952e-b78b", 
+            "date": "2013-04-01"
+        }, 
+        {
+            "amount": "200.00", 
+            "category": "BudgetCategoryImpl-13d5c659e0a-481f-b7c9", 
             "date": "2013-03-01"
         }, 
         {
             "amount": "10.00", 
-            "category": "6e4b612b-272f-42f0-a280-1701946ebf1e", 
+            "category": "BudgetCategoryImpl-13d5c659e0a-dd7a-1fc3", 
+            "date": "2013-03-01"
+        }, 
+        {
+            "amount": "500.00", 
+            "category": "BudgetCategoryImpl-13d5c659e0a-8e20-e5c", 
+            "date": "2013-03-01"
+        }, 
+        {
+            "amount": "400.00", 
+            "category": "BudgetCategoryImpl-13d5c659e0a-8e20-e5c", 
             "date": "2013-04-01"
         }, 
         {
-            "amount": "0.00", 
-            "category": "138b95ff-4425-4eaa-9997-627c4f230a6a", 
+            "amount": "300.00", 
+            "category": "BudgetCategoryImpl-13d5c659e0a-d2a0-9e81", 
+            "date": "2013-03-01"
+        }, 
+        {
+            "amount": "300.00", 
+            "category": "BudgetCategoryImpl-13d5c659e0a-d2a0-9e81", 
             "date": "2013-04-01"
         }, 
         {
-            "amount": "100.00", 
-            "category": "d52f3bce-72bc-42fb-a80a-4f9aaef1e95f", 
-            "date": "2013-04-01"
+            "amount": "90.00", 
+            "category": "BudgetCategoryImpl-13d5c659e0a-bf50-50d8", 
+            "date": "2013-03-01"
         }, 
         {
-            "amount": "600.00", 
-            "category": "4378588d-e8ef-443b-88f2-81fc9277ac46", 
+            "amount": "90.00", 
+            "category": "BudgetCategoryImpl-13d5c659e0a-bf50-50d8", 
             "date": "2013-04-01"
         }
     ], 
     "transactions": [
         {
-            "date": "2013-03-10", 
+            "date": "2013-03-11", 
             "deleted": true, 
-            "description": "Wal-Mart", 
+            "description": "Foo", 
             "number": "", 
             "splits": [
                 {
-                    "amount": "42.54", 
-                    "from": "3a26c3ea-f101-4f35-86a7-7b3ba438a79e", 
+                    "amount": "123.45", 
+                    "from": "AccountImpl-13d5c65dd62-224-3b69", 
                     "memo": "", 
-                    "to": "4378588d-e8ef-443b-88f2-81fc9277ac46"
+                    "to": "BudgetCategoryImpl-13d5c659e0a-dd7a-1fc3"
                 }
             ], 
-            "uuid": "022fc0f1-309e-4faf-8650-7808707e7e18"
+            "uuid": "4507645c-dbb1-4a31-827d-d3bd143d93f7"
         }, 
         {
-            "date": "2013-03-09", 
+            "date": "2013-03-11", 
             "deleted": true, 
-            "description": "Esso", 
+            "description": "Mortgage Payment", 
             "number": "", 
             "splits": [
                 {
-                    "amount": "10.00", 
-                    "from": "3a26c3ea-f101-4f35-86a7-7b3ba438a79e", 
+                    "amount": "2000.00", 
+                    "from": "AccountImpl-13d5c695c01-7de8-1251", 
                     "memo": "", 
-                    "to": "138b95ff-4425-4eaa-9997-627c4f230a6a"
+                    "to": "BudgetCategoryImpl-13d5c659e0a-cb-662c"
                 }
             ], 
-            "uuid": "904957e4-2886-4ae2-9b1a-2ce114aecea7"
+            "uuid": "TransactionImpl-13d5c6b0330-363f-ea9c"
         }, 
         {
-            "date": "2013-03-08", 
+            "date": "2013-03-11", 
             "deleted": true, 
-            "description": "Esso", 
+            "description": "Co-op", 
             "number": "", 
             "splits": [
                 {
-                    "amount": "5.00", 
-                    "from": "3a26c3ea-f101-4f35-86a7-7b3ba438a79e", 
+                    "amount": "102.12", 
+                    "from": "AccountImpl-13d5c65dd62-224-3b69", 
                     "memo": "", 
-                    "to": "d52f3bce-72bc-42fb-a80a-4f9aaef1e95f"
+                    "to": "BudgetCategoryImpl-13d5c659e0a-8e20-e5c"
                 }
             ], 
-            "uuid": "c017a7e9-64fd-4e47-b3f7-e8115cb2dce1"
+            "uuid": "TransactionImpl-13d5c690e68-1908-484e"
+        }, 
+        {
+            "date": "2013-03-05", 
+            "deleted": true, 
+            "description": "Cash Withdrawl", 
+            "number": "", 
+            "splits": [
+                {
+                    "amount": "100.00", 
+                    "from": "AccountImpl-13d5c695c01-7de8-1251", 
+                    "memo": "", 
+                    "to": "AccountImpl-13d5c65dd62-224-3b69"
+                }
+            ], 
+            "uuid": "b4d94b44-2868-4319-9828-5368746f64b1"
+        }, 
+        {
+            "date": "2013-03-03", 
+            "deleted": true, 
+            "description": "Cash Withdrawl", 
+            "number": "", 
+            "splits": [
+                {
+                    "amount": "500.00", 
+                    "from": "AccountImpl-13d5c695c01-7de8-1251", 
+                    "memo": "", 
+                    "to": "AccountImpl-13d5c65dd62-224-3b69"
+                }
+            ], 
+            "uuid": "TransactionImpl-13d5c6a8a84-547d-a1f"
+        }, 
+        {
+            "date": "2013-03-01", 
+            "deleted": true, 
+            "description": "Foo", 
+            "number": "", 
+            "splits": [
+                {
+                    "amount": "200.52", 
+                    "from": "AccountImpl-13d5c65dd62-224-3b69", 
+                    "memo": "", 
+                    "to": "BudgetCategoryImpl-13d5c659e0a-dd7a-1fc3"
+                }
+            ], 
+            "uuid": "TransactionImpl-13d5c6a0d72-b211-cf04"
+        }, 
+        {
+            "date": "2013-02-28", 
+            "deleted": true, 
+            "description": "Cash Withdrawl", 
+            "number": "", 
+            "splits": [
+                {
+                    "amount": "250.00", 
+                    "from": "AccountImpl-13d5c695c01-7de8-1251", 
+                    "memo": "", 
+                    "to": "AccountImpl-13d5c65dd62-224-3b69"
+                }
+            ], 
+            "uuid": "6932b816-3436-462c-a7a7-7f5ee697c7c8"
         }
     ]
 }
-' | curl -d @- -u 'foo@example.com:password' http://localhost:8080/data/restore | jsonlint; echo ''
+' | curl -d @- -u 'wyatt.olson@gmail.com:password' http://localhost:8080/data/restore | jsonlint; echo ''
