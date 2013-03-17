82d867f6061f50a43ff475afce1790d12807b56c
Wyatt <wyatt@digitalcave.ca>
2013-03-16 20:47:15 -0600

    Decrypt account types in the sources combo boxes

--- src/ca/digitalcave/buddi/live/resource/buddilive/SourcesResource.java
+++ src/ca/digitalcave/buddi/live/resource/buddilive/SourcesResource.java
@@ -2,6 +2,8 @@ package ca.digitalcave.buddi.live.resource.buddilive;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.ibatis.session.SqlSession;
@@ -39,7 +41,18 @@ public class SourcesResource extends ServerResource {
 		final User user = (User) getRequest().getClientInfo().getUser();
 		try {
 			final boolean isIncome = getRequest().getResourceRef().getBaseRef().toString().endsWith("from");
-			final List<AccountType> accountTypes = sqlSession.getMapper(Sources.class).selectAccountTypes(user);
+			final List<AccountType> accountsByType = sqlSession.getMapper(Sources.class).selectAccountTypes(user);
+			//If the data is encrypted, we cannot rely on the DB to sort the account types properly.  Thus we have to.
+			final Map<String, AccountType> accountTypeMap = new TreeMap<String, AccountType>();
+			for (AccountType accountType : accountsByType) {
+				final String key = (accountType.isDebit() ? "1" : "2") + CryptoUtil.decryptWrapper(accountType.getAccountType(), user);
+				if (!accountTypeMap.containsKey(key)) {
+					accountTypeMap.put(key, accountType);
+				}
+				else {
+					accountTypeMap.get(key).getAccounts().addAll(accountType.getAccounts());
+				}
+			}
 			final List<Category> categories = Category.getHierarchy(sqlSession.getMapper(Sources.class).selectCategories(user, isIncome));
 			final JSONObject result = new JSONObject();
 
@@ -51,11 +64,12 @@ public class SourcesResource extends ServerResource {
 			separator.put("style", "color: " + FormatUtil.HTML_GRAY + ";");
 			result.append("data", separator);
 			
-			for (AccountType at : accountTypes) {
+			for (String key : accountTypeMap.keySet()) {
+				AccountType at = accountTypeMap.get(key);
 				if (!at.isDeleted() || user.isShowDeleted()){
 					final JSONObject accountType = new JSONObject();
 					accountType.put("value", "");
-					accountType.put("text", at.getAccountType());
+					accountType.put("text", CryptoUtil.decryptWrapper(at.getAccountType(), user));
 					if (at.isDeleted()) sb.append(" text-decoration: line-through;");
 					if (!at.isDebit()) sb.append(" color: " + FormatUtil.HTML_RED + ";");
 					accountType.put("style", sb.toString());
