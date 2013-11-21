48ca19b3438186a390c5ab33b78b15ae2439163c
Wyatt <wyatt@digitalcave.ca>
2013-11-20 17:03:18 -0700

    Starting to integrate the /login router (WIP as we develop the moss.restlet implementation providing it)

--- src/ca/digitalcave/buddi/live/BuddiApplication.java
+++ src/ca/digitalcave/buddi/live/BuddiApplication.java
@@ -1,6 +1,7 @@
 package ca.digitalcave.buddi.live;
 
 import java.security.Key;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Properties;
 
@@ -51,6 +52,7 @@ import ca.digitalcave.moss.crypto.Crypto;
 import ca.digitalcave.moss.crypto.Crypto.Algorithm;
 import ca.digitalcave.moss.crypto.Crypto.CryptoException;
 import ca.digitalcave.moss.restlet.CookieAuthenticator;
+import ca.digitalcave.moss.restlet.login.LoginRouter;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
@@ -170,6 +172,10 @@ public class BuddiApplication extends Application{
 		publicRouter.attach("/", new Redirector(getContext(), "index.html", Redirector.MODE_CLIENT_TEMPORARY));
 		publicRouter.attach("/index", IndexResource.class);
 		publicRouter.attach("/data", privateRouter);
+		final HashMap<String, Object> loginConfig = new HashMap<String, Object>();
+		loginConfig.put("extraLoginPanelFields", "{ 'fieldLabel': 'Foo', 'inputType': 'password', 'name': 'secret' },");
+		loginConfig.put("showEnrole", false);
+		publicRouter.attach("/login", new LoginRouter(loginConfig));
 		publicRouter.attachDefault(DefaultResource.class).setMatchingMode(Template.MODE_STARTS_WITH);
 		
 		final Encoder encoder = new Encoder(getContext(), false, true, getEncoderService());
