407da83e546ecc5692f2a114cbf593d68f571021
Wyatt <wyatt@digitalcave.ca>
2014-02-28 12:10:25 -0700

    Split out activation keys into its own table, to better track activation key expiry.  Fixed some bugs in registration and password reset.  WARNING: if you apply this change to a production system, you will need to manually modify the database by either dropping all tables and re-creating automatically (and then restoring the data), or by manually changing the liquibase migration tables (databasechangelog) to only see the new change log IDs and MD5 hashes, and then remove the activation_key column from users table.

--- src/ca/digitalcave/buddi/live/db/Users.java
+++ src/ca/digitalcave/buddi/live/db/Users.java
@@ -7,16 +7,20 @@ import ca.digitalcave.buddi.live.model.User;
 
 public interface Users {
 	public User selectUser(@Param("identifier") String identifier);
+	public User selectUserByActivationKey(@Param("activationKey") String activationKey);
 	public int selectEncryptionVersion(@Param("user") User user);
 	
-	public int insertUser(@Param("user") User user, @Param("activationKey") String activationKey);
+	public int insertUser(@Param("user") User user);
+	public int insertActivationKey(@Param("user") User user, @Param("activationKey") String activationKey);
 	
 	public int updateUserLoginTime(@Param("user") User user);
 	public int updateUser(@Param("user") User user);
 	public int updateUserEncryptionKey(@Param("user") User user);
-	public int updateUserActivationKey(@Param("hashedIdentifier") String hashedIdentifier, @Param("activationKey") String activationKey);
-	public int updateUserSecret(@Param("activationKey") String activationKey, @Param("hashedSecret") String hashedSecret);
+	public int updateUserSecret(@Param("user") User user, @Param("hashedSecret") String hashedSecret);
 	public int updateUserEncryptionVersion(@Param("user") User user, @Param("encryptionVersion") int encryptionVersion);
 	
-	public Integer deleteUser(@Param("user") User user);
+	public int deleteUser(@Param("user") User user);
+	public int deleteActivationKey();
+	public int deleteActivationKey(@Param("user") User user);
+	public int deleteInactiveUsers();
 }
--- src/ca/digitalcave/buddi/live/resource/IndexResource.java
+++ src/ca/digitalcave/buddi/live/resource/IndexResource.java
@@ -88,9 +88,13 @@ public class IndexResource extends CookieAuthInterceptResource {
 			newUser.setCurrency(Currency.getInstance(form.getFirstValue("currency", "USD")));
 			newUser.setLocale(LocaleUtils.toLocale(form.getFirstValue("locale", "en_US")));
 			ConstraintsChecker.checkInsertUser(newUser, sqlSession);
+
+			cleanupUsers(sqlSession, null);
 			
-			final Integer count = sqlSession.getMapper(Users.class).insertUser(newUser, activationKey);
-			if (count != 1) throw new DatabaseException(String.format("Insert failed; expected 1 row, returned %s", count));
+			final Integer insertUserCount = sqlSession.getMapper(Users.class).insertUser(newUser);
+			if (insertUserCount != 1) throw new DatabaseException(String.format("User insert failed; expected 1 row, returned %s", insertUserCount));
+			final Integer insertActivationCount = sqlSession.getMapper(Users.class).insertActivationKey(newUser, activationKey);
+			if (insertActivationCount != 1) throw new DatabaseException(String.format("Activation key insert failed; expected 1 row, returned %s", insertActivationCount));
 			
 			sqlSession.commit();
 
@@ -109,7 +113,14 @@ public class IndexResource extends CookieAuthInterceptResource {
 		final BuddiApplication application = (BuddiApplication) getApplication();
 		final SqlSession sqlSession = application.getSqlSessionFactory().openSession();
 		try {
-			final Integer count = sqlSession.getMapper(Users.class).updateUserActivationKey(new MossHash().setSaltLength(0).setIterations(1).generate(identifier), activationKey);
+			final String hashedIdentifier = new MossHash().setSaltLength(0).setIterations(1).generate(identifier);
+			final User user = sqlSession.getMapper(Users.class).selectUser(hashedIdentifier);
+			if (user == null) throw new DatabaseException("Could not find user with hashed identifier" + hashedIdentifier);
+			if (user.isEncrypted()) throw new DatabaseException("Users with encrypted data cannot reset passwords.");
+			
+			cleanupUsers(sqlSession, user);
+			
+			final Integer count = sqlSession.getMapper(Users.class).insertActivationKey(user, activationKey);
 			if (count != 1) throw new DatabaseException(String.format("Insert failed; expected 1 row, returned %s", count));
 			
 			sqlSession.commit();
@@ -129,8 +140,12 @@ public class IndexResource extends CookieAuthInterceptResource {
 		final BuddiApplication application = (BuddiApplication) getApplication();
 		final SqlSession sqlSession = application.getSqlSessionFactory().openSession();
 		try {
-			final Integer count = sqlSession.getMapper(Users.class).updateUserSecret(activationKey, hash);
+			final User user = sqlSession.getMapper(Users.class).selectUserByActivationKey(activationKey);
+			if (user == null) throw new DatabaseException("Activation key is not valid");
+			final Integer count = sqlSession.getMapper(Users.class).updateUserSecret(user, hash);
 			if (count != 1) throw new DatabaseException(String.format("Update failed; expected 1 row, returned %s", count));
+			
+			cleanupUsers(sqlSession, user);
 			
 			sqlSession.commit();
 		} catch (DatabaseException e){
@@ -148,5 +163,11 @@ public class IndexResource extends CookieAuthInterceptResource {
 	@Override
 	protected boolean isAllowReset() {
 		return true;
+	}
+	
+	private void cleanupUsers(SqlSession sqlSession, User user) throws DatabaseException {
+		if (user == null || user.getId() == null) sqlSession.getMapper(Users.class).deleteActivationKey();	//Delete keys older than one day
+		else  sqlSession.getMapper(Users.class).deleteActivationKey(user);	//Delete any existing keys for user, and all keys older than one day
+		sqlSession.getMapper(Users.class).deleteInactiveUsers();	//Delete users who never set a password and whose activation keys have already expired (i.e. they never completed registration and it is too late to do so now)
 	}
 }
