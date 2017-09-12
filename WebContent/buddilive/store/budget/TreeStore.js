5eba9ea62436cf6e675f921611e8490a40aa742d
Wyatt <wyatt@digitalcave.ca>
2017-09-12 09:47:46 -0600

    Fix some bugs with budget category, and add totals to average income and expenses report

--- WebContent/buddilive/store/budget/TreeStore.js
+++ WebContent/buddilive/store/budget/TreeStore.js
@@ -17,22 +17,26 @@ Ext.define("BuddiLive.store.budget.TreeStore", {
 		"difference",
 		"differenceStyle"
 	],
-	"proxy": {
-		"type": "ajax",
-		"url": "data/categories.json?periodType=MONTH",
-		"reader": {
-			"type": "json",
-			"rootProperty": "children",
-			"transform": function(data){
-				budgetTree = Ext.ComponentQuery.query('viewport')[0].down("budgettree");
-				budgetTree.down("textfield[itemId='currentPeriod']").setValue(data.period);
-				var columns = budgetTree.getView().headerCt.items.items;
-				columns[1].setText(columns[1].initialConfig.text + " (" + data.previousPeriod + ")");
-				columns[2].setText(columns[2].initialConfig.text + " (" + data.period + ")");
-				budgetTree.currentDate = data.date;	//ISO Date string, will be used as current reference when passing nextPeriod / previousPeriod
-				return data;	//We don't actually transform here, we just need a hook to get access to the raw data.  Return data unmolested.
+	"autoLoad": true,
+	"constructor": function(config){
+		config.proxy = {
+			"type": "ajax",
+			"url": "data/categories.json?periodType=" + config.periodType,
+			"reader": {
+				"type": "json",
+				"rootProperty": "children",
+				"transform": function(data){
+					debugger;
+					budgetTree = Ext.ComponentQuery.query('viewport')[0].down("budgettree[itemId='" + config.periodType + "']");
+					budgetTree.down("textfield[itemId='currentPeriod']").setValue(data.period);
+					var columns = budgetTree.getView().headerCt.items.items;
+					columns[1].setText(columns[1].initialConfig.text + " (" + data.previousPeriod + ")");
+					columns[2].setText(columns[2].initialConfig.text + " (" + data.period + ")");
+					budgetTree.currentDate = data.date;	//ISO Date string, will be used as current reference when passing nextPeriod / previousPeriod
+					return data;	//We don't actually transform here, we just need a hook to get access to the raw data.  Return data unmolested.
+				}
 			}
-		}
-	},
-	"autoload": true
+		};
+		this.callParent(arguments);
+	}
 });
--- src/ca/digitalcave/buddi/live/resource/buddilive/report/AverageIncomeAndExpensesByCategoryResource.java
+++ src/ca/digitalcave/buddi/live/resource/buddilive/report/AverageIncomeAndExpensesByCategoryResource.java
@@ -10,6 +10,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.ibatis.session.SqlSession;
 import org.json.JSONException;
@@ -51,95 +52,11 @@ public class AverageIncomeAndExpensesByCategoryResource extends ServerResource {
 		try {
 			final Date[] dates = ReportHelper.processInterval(getQuery());
 			final List<Transaction> transactions = sqlSession.getMapper(Transactions.class).selectTransactions(user, dates[0], dates[1]);
-			final List<Category> categories = sqlSession.getMapper(Sources.class).selectCategories(user);
-			final int totalDaysInRange = DateUtil.getDaysBetween(dates[0], dates[1], true);
-			
-			//Sort collections by income, name.  We can't do this in the DB due to encrypted names.
-			Collections.sort(categories, new Comparator<Category>() {
-				@Override
-				public int compare(Category o1, Category o2) {
-					if (o1 == null || o2 == null) return 0;
-					
-					if (o1.isIncome() != o2.isIncome()) return o1.isIncome() ? -1 : 1;
-					
-					try {
-						return CryptoUtil.decryptWrapper(o1.getName(), user).compareTo(CryptoUtil.decryptWrapper(o2.getName(), user));
-					}
-					catch (CryptoException e){
-						return 0;
-					}
-				}
-			});
-			
-			//Map all the transactions by source ID and sum them.  We could sum in the DB, but we do it this way elsewhere so we may as well just do this in memory.
-			final Map<Integer, BigDecimal> totalsBySource = new HashMap<Integer, BigDecimal>();
-			for (Transaction transaction : transactions) {
-				for (Split split : transaction.getSplits()) {
-					//A split can only be either from or to a category; not both.  Find which it is, and
-					// add it to that category in the map.
-					if ("I".equals(split.getFromType()) || "E".equals(split.getFromType())){
-						//Ensure there is a list in the map already
-						final int source = split.getFromSource();
-
-						//Create a new transaction, containing only this one split.
-						final Transaction t = new Transaction();
-						t.setDate(transaction.getDate());
-						t.setDescription(transaction.getDescription());
-						t.setNumber(transaction.getNumber());
-						t.setSplits(new ArrayList<Split>());
-						t.getSplits().add(split);
-
-						//Sum the balances
-						final BigDecimal splitAmount = CryptoUtil.decryptWrapperBigDecimal(split.getAmount(), user, true);
-						totalsBySource.put(source, splitAmount.add(totalsBySource.get(source) == null ? BigDecimal.ZERO : totalsBySource.get(source)));
-					}
-					else if ("I".equals(split.getToType()) || "E".equals(split.getToType())){
-						//Ensure there is a list in the map already
-						final int source = split.getToSource();
-
-						//Create a new transaction, containing only this one split.
-						final Transaction t = new Transaction();
-						t.setDate(transaction.getDate());
-						t.setDescription(transaction.getDescription());
-						t.setNumber(transaction.getNumber());
-						t.setSplits(new ArrayList<Split>());
-						t.getSplits().add(split);
-
-						//Sum the balances
-						final BigDecimal splitAmount = CryptoUtil.decryptWrapperBigDecimal(split.getAmount(), user, true);
-						totalsBySource.put(source, splitAmount.add(totalsBySource.get(source) == null ? BigDecimal.ZERO : totalsBySource.get(source)));
-					}
-				}
-			}
 			
 			final JSONObject result = new JSONObject();
-			for (Category category : categories) {
-				final BigDecimal budgetedAmount = category.getAmount(user, sqlSession, dates[0], dates[1]);
-				final BigDecimal actualAmount = totalsBySource.get(category.getId()) == null ? BigDecimal.ZERO : totalsBySource.get(category.getId());
-				
-				if (budgetedAmount.compareTo(BigDecimal.ZERO) != 0 || actualAmount.compareTo(BigDecimal.ZERO) != 0){
-					final JSONObject object = new JSONObject();
-					object.put("source", CryptoUtil.decryptWrapper(category.getName(), user));
-
-					final BigDecimal daysInPeriod = new BigDecimal(CategoryPeriods.valueOf(category.getPeriodType()).getDaysInPeriod(dates[0]));
-					final BigDecimal averageAmount = actualAmount.divide(new BigDecimal(totalDaysInRange), RoundingMode.HALF_UP).multiply(daysInPeriod);
-					object.put("average", FormatUtil.formatCurrency(averageAmount, user));
-					object.put("averageStyle", (FormatUtil.isRed(category, averageAmount) ? FormatUtil.formatRed() : ""));
-					
-					final BigDecimal averageBudgeted = budgetedAmount.divide(new BigDecimal(totalDaysInRange), RoundingMode.HALF_UP).multiply(daysInPeriod);
-					object.put("averageBudgeted", FormatUtil.formatCurrency(averageBudgeted, user));
-					object.put("averageBudgetedStyle", (FormatUtil.isRed(category, averageBudgeted) ? FormatUtil.formatRed() : ""));
-					
-					final BigDecimal difference = (averageAmount.subtract(averageBudgeted));
-					object.put("difference", FormatUtil.formatCurrency(difference, user));
-					object.put("differenceStyle", (FormatUtil.isRed(category, difference) ? FormatUtil.formatRed() : ""));
-
-					object.put("period", LocaleUtil.getTranslation(getRequest()).getString("BUDGET_CATEGORY_TYPE_" + category.getPeriodType()));
-					object.put("periodStyle", (FormatUtil.isRed(category, actualAmount) ? FormatUtil.formatRed() : ""));
-
-					result.append("data", object);
-				}
-			}
+			
+			calculateCategories(result, true, user, sqlSession, sqlSession.getMapper(Sources.class).selectCategories(user, true), transactions, dates);
+			calculateCategories(result, false, user, sqlSession, sqlSession.getMapper(Sources.class).selectCategories(user, false), transactions, dates);
 
 			result.put("success", true);
 			return new JsonRepresentation(result);
@@ -158,6 +75,122 @@ public class AverageIncomeAndExpensesByCategoryResource extends ServerResource {
 		}
 		finally {
 			sqlSession.close();
+		}
+	}
+	
+	private void calculateCategories(JSONObject result, boolean income, final User user, SqlSession sqlSession, List<Category> categories, List<Transaction> transactions, Date[] dates) throws JSONException, CryptoException, SQLException{
+		//Sort collections by income, name.  We can't do this in the DB due to encrypted names.
+		Collections.sort(categories, new Comparator<Category>() {
+			@Override
+			public int compare(Category o1, Category o2) {
+				if (o1 == null || o2 == null) return 0;
+				
+				if (o1.isIncome() != o2.isIncome()) return o1.isIncome() ? -1 : 1;
+				
+				try {
+					return CryptoUtil.decryptWrapper(o1.getName(), user).compareTo(CryptoUtil.decryptWrapper(o2.getName(), user));
+				}
+				catch (CryptoException e){
+					return 0;
+				}
+			}
+		});
+		
+		//Map all the transactions by source ID and sum them.  We could sum in the DB, but we do it this way elsewhere so we may as well just do this in memory.
+		final Map<Integer, BigDecimal> totalsBySource = new HashMap<Integer, BigDecimal>();
+		for (Transaction transaction : transactions) {
+			for (Split split : transaction.getSplits()) {
+				//A split can only be either from or to a category; not both.  Find which it is, and
+				// add it to that category in the map.
+				if ("I".equals(split.getFromType()) || "E".equals(split.getFromType())){
+					//Ensure there is a list in the map already
+					final int source = split.getFromSource();
+
+					//Create a new transaction, containing only this one split.
+					final Transaction t = new Transaction();
+					t.setDate(transaction.getDate());
+					t.setDescription(transaction.getDescription());
+					t.setNumber(transaction.getNumber());
+					t.setSplits(new ArrayList<Split>());
+					t.getSplits().add(split);
+
+					//Sum the balances
+					final BigDecimal splitAmount = CryptoUtil.decryptWrapperBigDecimal(split.getAmount(), user, true);
+					totalsBySource.put(source, splitAmount.add(totalsBySource.get(source) == null ? BigDecimal.ZERO : totalsBySource.get(source)));
+				}
+				else if ("I".equals(split.getToType()) || "E".equals(split.getToType())){
+					//Ensure there is a list in the map already
+					final int source = split.getToSource();
+
+					//Create a new transaction, containing only this one split.
+					final Transaction t = new Transaction();
+					t.setDate(transaction.getDate());
+					t.setDescription(transaction.getDescription());
+					t.setNumber(transaction.getNumber());
+					t.setSplits(new ArrayList<Split>());
+					t.getSplits().add(split);
+
+					//Sum the balances
+					final BigDecimal splitAmount = CryptoUtil.decryptWrapperBigDecimal(split.getAmount(), user, true);
+					totalsBySource.put(source, splitAmount.add(totalsBySource.get(source) == null ? BigDecimal.ZERO : totalsBySource.get(source)));
+				}
+			}
+		}
+		
+		final Map<String, BigDecimal> totalActualByPeriod = new TreeMap<String, BigDecimal>();
+		final Map<String, BigDecimal> totalBudgetedByPeriod = new TreeMap<String, BigDecimal>();
+		final int totalDaysInRange = DateUtil.getDaysBetween(dates[0], dates[1], true);
+		
+		for (Category category : categories) {
+			final BigDecimal budgetedAmount = category.getAmount(user, sqlSession, dates[0], dates[1]);
+			final BigDecimal actualAmount = totalsBySource.get(category.getId()) == null ? BigDecimal.ZERO : totalsBySource.get(category.getId());
+			
+			if (budgetedAmount.compareTo(BigDecimal.ZERO) != 0 || actualAmount.compareTo(BigDecimal.ZERO) != 0){
+				final JSONObject object = new JSONObject();
+				object.put("source", CryptoUtil.decryptWrapper(category.getName(), user));
+				
+				final BigDecimal daysInPeriod = new BigDecimal(CategoryPeriods.valueOf(category.getPeriodType()).getDaysInPeriod(dates[0]));
+				final BigDecimal averageAmount = actualAmount.divide(new BigDecimal(totalDaysInRange), RoundingMode.HALF_UP).multiply(daysInPeriod);
+				object.put("average", FormatUtil.formatCurrency(averageAmount, user));
+				object.put("averageStyle", (FormatUtil.isRed(category, averageAmount) ? FormatUtil.formatRed() : ""));
+				
+				final BigDecimal averageBudgeted = budgetedAmount.divide(new BigDecimal(totalDaysInRange), RoundingMode.HALF_UP).multiply(daysInPeriod);
+				object.put("averageBudgeted", FormatUtil.formatCurrency(averageBudgeted, user));
+				object.put("averageBudgetedStyle", (FormatUtil.isRed(category, averageBudgeted) ? FormatUtil.formatRed() : ""));
+				
+				final BigDecimal difference = (averageAmount.subtract(averageBudgeted));
+				object.put("difference", FormatUtil.formatCurrency(difference, user));
+				object.put("differenceStyle", (FormatUtil.isRed(category, difference) ? FormatUtil.formatRed() : ""));
+
+				object.put("period", LocaleUtil.getTranslation(getRequest()).getString("BUDGET_CATEGORY_TYPE_" + category.getPeriodType()));
+				object.put("periodStyle", (FormatUtil.isRed(category, actualAmount) ? FormatUtil.formatRed() : ""));
+				
+				totalActualByPeriod.put(category.getPeriodType(), (totalActualByPeriod.get(category.getPeriodType()) == null ? BigDecimal.ZERO : totalActualByPeriod.get(category.getPeriodType())).add(averageAmount));
+				totalBudgetedByPeriod.put(category.getPeriodType(), (totalBudgetedByPeriod.get(category.getPeriodType()) == null ? BigDecimal.ZERO : totalBudgetedByPeriod.get(category.getPeriodType())).add(averageBudgeted));
+
+				result.append("data", object);
+			}
+		}
+		
+		for (String period : totalActualByPeriod.keySet()) {
+			final JSONObject object = new JSONObject();
+			object.put("source", LocaleUtil.getTranslation(getRequest()).getString(income ? "AVERAGE_INCOME" : "AVERAGE_EXPENSES") + " / " + LocaleUtil.getTranslation(getRequest()).getString("BUDGET_CATEGORY_TYPE_" + period));
+			object.put("sourceStyle", "font-weight: bold;");
+			
+			object.put("average", FormatUtil.formatCurrency(totalActualByPeriod.get(period), user));
+			object.put("averageStyle", "font-weight: bold;" + (totalActualByPeriod.get(period).compareTo(BigDecimal.ZERO) >= 0 ? FormatUtil.formatRed() : ""));
+			
+			object.put("averageBudgeted", FormatUtil.formatCurrency(totalBudgetedByPeriod.get(period), user));
+			object.put("averageBudgetedStyle", "font-weight: bold;" + (totalBudgetedByPeriod.get(period).compareTo(BigDecimal.ZERO) >= 0 ? FormatUtil.formatRed() : ""));
+			
+			final BigDecimal difference = (totalActualByPeriod.get(period).subtract(totalBudgetedByPeriod.get(period)));
+			object.put("difference", FormatUtil.formatCurrency(difference, user));
+			object.put("differenceStyle", "font-weight: bold;" + (FormatUtil.isRed(difference) ? FormatUtil.formatRed() : ""));
+
+			object.put("period", LocaleUtil.getTranslation(getRequest()).getString("BUDGET_CATEGORY_TYPE_" + period));
+			object.put("periodStyle", "font-weight: bold;" + FormatUtil.formatRed());
+
+			result.append("data", object);
 		}
 	}
 }
